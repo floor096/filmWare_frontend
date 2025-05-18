6c57542e60c77a55919d7ffc53a171c284f2e873
Flor <florencia.ortiz096@gmail.com>
2025-05-18 20:17:48 -0300

    actualizacion

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// ----------  MAIN Y LOGIN  ---------------------
 // ========================
 // Animación de tarjetas (index.html)
 // ========================
@@ -150,6 +151,50 @@ document.addEventListener('DOMContentLoaded', function () {
                 window.location.href = "index.html";
             }
         });
+    }
+
+    // ========================
+    // Login Usuario (nabvar)
+    // ========================
+
+    // Simulación de datos de sesión (esto luego vendrá del backend o localStorage)
+    const usuarioLogueado = JSON.parse(localStorage.getItem('usuarioFilmWare'));
+    // Ejemplo: { nombre: "Ana", tipo: "cliente" } o { nombre: "Luis", tipo: "admin" }
+    // Guardar sesión ficticia
+
+
+    if (usuarioLogueado) {
+        document.getElementById('btnLogin').classList.add('d-none');
+        document.getElementById('btnRegister').classList.add('d-none');
+
+        const userDropdown = document.getElementById('userDropdown');
+        const userName = document.getElementById('userName');
+        const userMenu = document.getElementById('userMenu');
+
+        userName.textContent = usuarioLogueado.nombre;
+        userDropdown.classList.remove('d-none');
+
+        if (usuarioLogueado.tipo === 'cliente') {
+            userMenu.innerHTML = `
+            <li><a class="dropdown-item" href="perfil.html">Perfil</a></li>
+            <li><a class="dropdown-item" href="compras.html">Mis Compras</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#" onclick="cerrarSesion()">Salir</a></li>
+            `;
+        } else if (usuarioLogueado.tipo === 'admin') {
+            userMenu.innerHTML = `
+            <li><a class="dropdown-item" href="perfil.html">Perfil</a></li>
+            <li><a class="dropdown-item" href="registro-cine.html">Cines y Sucursales</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#" onclick="cerrarSesion()">Salir</a></li>
+            `;
+        }
+    }
+
+    // Función de cerrar sesión
+    function cerrarSesion() {
+        localStorage.removeItem('usuarioFilmWare');
+        window.location.href = 'index.html';
     }
     
 });
